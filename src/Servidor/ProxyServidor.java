8c11231a01db8c065c240b631783f7692595d608
NoeCob <gandalfr18@gmail.com>
2013-10-22 16:21:52 -0500

    Grafica funcional

--- src/Servidor/ProxyServidor.java
+++ src/Servidor/ProxyServidor.java
@@ -16,19 +16,18 @@ public class ProxyServidor {
     private XStream xStream = new XStream(new DomDriver());
     private ControaladorServidor control;
     private GraficaBarras graficaBarras;
+    private ArrayList <Candidato> candidatos;
 
     public ProxyServidor() {
         control = new ControaladorServidor();
         graficaBarras = new GraficaBarras();
         graficaBarras.setVisible(true);
+        candidatos = new ArrayList<Candidato>();
     }
 
     public void recibirMensaje(String mensaje) throws FileNotFoundException, IOException {
         this.mensaje = mensaje;
         convertirMensaje(mensaje);
-        
-
-
     }
 
     //Revisar la entrada de la variable
@@ -42,29 +41,19 @@ public class ProxyServidor {
         System.out.println("Candidato" +candidato.getNombre());
 //        Error de la conversión de xml a clase
 //
-//        File xmlFile = new File("");
-//        xStream.fromXML(new FileInputStream(xmlFile));
-//
-//        XStream xstream = new XStream();
-//        xstream.useAttributeFor(String.class);
-//        xstream.useAttributeFor(Integer.class);
-//
-//        Writer writer = new FileWriter(xmlFile);
-//        writer.write(xstream.toXML(xmlFile));
-//        writer.close();
-        
-        
 
+        
 //        System.out.println((Candidato) xStream.fromXML(mensajeRecibido.substring(1)));
 
 //        (Candidato)xStream.fromXML(mensajeRecibido);
 
 
         //Enviar a la vista
-//
-//        control.contabilizarVoto(candidato);
-//        candidatos = control.darVotosContabilizados();
-//        graficaBarras.actualizar(candidatos);
+
+        
+        control.contabilizarVoto(candidato);
+        candidatos = control.darVotosContabilizados();
+        graficaBarras.actualizar(candidatos);
 
 
 
